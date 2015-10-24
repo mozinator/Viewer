d1c8a6c0f8ce0cfa02d846c1baf1ae23eb724477
Fabian Aussems <fabianaussems@gmail.com>
2015-10-24 17:18:28 +0200

    added framework target

---
+++ ViewerFramework/ViewerFramework.h
@@ -0,0 +1,19 @@
+//
+//  ViewerFramework.h
+//  ViewerFramework
+//
+//  Created by mozinator on 10/24/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ViewerFramework.
+FOUNDATION_EXPORT double ViewerFrameworkVersionNumber;
+
+//! Project version string for ViewerFramework.
+FOUNDATION_EXPORT const unsigned char ViewerFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ViewerFramework/PublicHeader.h>
+
+
