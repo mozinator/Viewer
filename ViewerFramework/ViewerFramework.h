71c3b7af535470792f916cac2f33afa6b6d481ba
Fabian Aussems <fabianaussems@gmail.com>
2015-10-24 18:36:58 +0200

    added headers to framework header

--- ViewerFramework/ViewerFramework.h
+++ ViewerFramework/ViewerFramework.h
@@ -15,5 +15,33 @@ FOUNDATION_EXPORT double ViewerFrameworkVersionNumber;
 FOUNDATION_EXPORT const unsigned char ViewerFrameworkVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <ViewerFramework/PublicHeader.h>
-
-
+#import <ViewerFramework/LibraryViewController.h>
+#import <ViewerFramework/LibraryDirectoryView.h>
+#import <ViewerFramework/LibraryDocumentsView.h>
+#import <ViewerFramework/FoldersViewController.h>
+#import <ViewerFramework/HelpViewController.h>
+#import <ViewerFramework/DocumentFolder.h>
+#import <ViewerFramework/ReaderDocument.h>
+#import <ViewerFramework/DocumentsUpdate.h>
+#import <ViewerFramework/CoreDataManager.h>
+#import <ViewerFramework/DirectoryWatcher.h>
+#import <ViewerFramework/UIXTextEntry.h>
+#import <ViewerFramework/ReaderConstants.h>
+#import <ViewerFramework/ReaderViewController.h>
+#import <ViewerFramework/ReaderMainToolbar.h>
+#import <ViewerFramework/ReaderMainPagebar.h>
+#import <ViewerFramework/ReaderContentView.h>
+#import <ViewerFramework/ReaderContentPage.h>
+#import <ViewerFramework/ReaderContentTile.h>
+#import <ViewerFramework/ThumbsViewController.h>
+#import <ViewerFramework/ThumbsMainToolbar.h>
+#import <ViewerFramework/CGPDFDocument.h>
+#import <ViewerFramework/ReaderDocumentOutline.h>
+#import <ViewerFramework/ReaderThumbCache.h>
+#import <ViewerFramework/ReaderThumbRequest.h>
+#import <ViewerFramework/ReaderThumbQueue.h>
+#import <ViewerFramework/ReaderThumbFetch.h>
+#import <ViewerFramework/ReaderThumbRender.h>
+#import <ViewerFramework/ReaderThumbsView.h>
+#import <ViewerFramework/ReaderThumbView.h>
+#import <ViewerFramework/UIXToolbarView.h>
